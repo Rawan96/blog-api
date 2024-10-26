979ac7ad60890636398636b3fe1d48a8622da3b3
rawan <rawan.dev6@gmail.com>
2024-10-26 03:09:05 +0300

    vercel

--- database/config/connection.js
+++ database/config/connection.js
@@ -1,9 +1,8 @@
 const { Pool } = require('pg');
-require('env2')('./config.env');
+require('env2')('./.env');
 
 if (!process.env.DB_URL) throw new Error('No Database URL!');
 
 module.exports = new Pool({
-  connectionString: process.env.DB_URL,
-  ssl: process.env.DATABASE_URL ? true : false,
+  connectionString: process.env.POSTGRES_URL,
 });
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('env2')('./config.env');
+require('env2')('./.env');
 const express = require('express');
 const postRoutes = require('./routes/postRoutes');
 
