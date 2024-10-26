56e91431a8a7dc03504167e48303847878d1854c
rawan <rawan.dev6@gmail.com>
2024-10-26 17:13:32 +0300

    Resolve conflict

--- database/config/connection.js
+++ database/config/connection.js
@@ -1,9 +1,10 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-if (!process.env.DB_URL) throw new Error('No Database URL!');
+console.log('DB_URL:', process.env.DATABASE_URL);
+
+if (!process.env.DATABASE_URL) throw new Error('No Database URL!');
 
 module.exports = new Pool({
-  connectionString: process.env.DB_URL,
-  ssl: process.env.DATABASE_URL ? true : false,
+  connectionString: process.env.DATABASE_URL,
 });
